01a4a6c183d43c3f7b212de3db2374eaca84b1e4
HMWAI <henry.wamunyu@gmail.com>
2023-04-19 21:25:56 +0300

    Team 1

--- group3.py
+++
@@ -1,7 +0,0 @@
-# This function is designed to fill null values in a column with the median of that column
-def fun_median_fill_null(df, column_name):
-    # First calculate the median value of a float
-    median_value = df[column_name].astype(float).median()
-    # Then fill the null values with the median value
-    df[column_name].fillna(value = median_value, inplace=True)
-    return df
